1375222ea2bd2ccbf40b2eb93fe56b3426add9ee
IgorPerikov <perikov.igor@gmail.com>
2017-08-26 15:42:57 +0300

    add pull request check

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
    docker build -t prodrink/prodrink-catalog .
 else
    ./gradlew build
