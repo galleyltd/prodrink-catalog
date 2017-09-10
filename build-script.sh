ac3cdeccb9aa580647da7897bd16d9948bcb25b4
IgorPerikov <perikov.igor@gmail.com>
2017-09-10 10:50:04 +0300

    update[1]

--- build-script.sh
+++ build-script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+./gradlew build
+export IMAGE_PATH=prodrink/prodrink-catalog
+export VERSION=`cat VERSION`.$TRAVIS_BUILD_NUMBER
+docker build -t $IMAGE_PATH:$VERSION .
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
-   export IMAGE_PATH=prodrink/prodrink-catalog
-   export VERSION=`cat VERSION`.$TRAVIS_BUILD_NUMBER
-   docker build -t $IMAGE_PATH:$VERSION .
    ./docker-push.sh
-else
-   ./gradlew build
 fi
--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker tag $IMAGE_PATH:latest $IMAGE_PATH:$VERSION
+docker tag $IMAGE_PATH:$VERSION
+docker tag $IMAGE_PATH:latest
 docker push $IMAGE_PATH:$VERSION
-docker push $IMAGE_PATH:latest
