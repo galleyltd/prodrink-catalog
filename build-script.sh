605ccb1faec19e3c15b8d428525b768914c9dcb7
IgorPerikov <perikov.igor@gmail.com>
2017-09-10 10:37:43 +0300

    fix envvar usage

--- build-script.sh
+++ build-script.sh
@@ -2,7 +2,7 @@
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
    export IMAGE_PATH=prodrink/prodrink-catalog
    export VERSION=`cat VERSION`.$TRAVIS_BUILD_NUMBER
-   docker build -t IMAGE_PATH:$VERSION .
+   docker build -t $IMAGE_PATH:$VERSION .
    ./docker-push.sh
 else
    ./gradlew build
